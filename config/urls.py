afae66635c6ee28dd9c8a19a7bf9abaade514f73
Alex <bmgula55@mail.ru>
2024-05-08 00:49:41 +0300

    + Documentation

--- config/urls.py
+++ config/urls.py
@@ -35,7 +35,10 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [path('admin/', admin.site.urls),
+urlpatterns = [path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+               path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+               path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+               path('admin/', admin.site.urls),
                path('', include('habits.urls', namespace='habits')),
                path('users/', include('users.urls', namespace='users')),
                ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
